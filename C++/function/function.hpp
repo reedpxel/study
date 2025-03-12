06bac77ed178f027781a6ef59e9eef1ea82ff085
reedpxel <reedpxel@yandex.ru>
2025-03-12 22:39:06 +0300

    copy and move ctors added

--- C++/function/function.hpp
+++ C++/function/function.hpp
@@ -1,4 +1,7 @@
 #include <iostream> // TO DO: remove
+#include <type_traits>
+#include <cstdint>
+#include <utility>
 #define BUFFER_SIZE 16
 
 /* With what function must be able to be initialized:
@@ -10,61 +13,159 @@
  * - std::bind
  */
 
+struct Debug
+{
+    template <typename T>
+    Debug(T) = delete;
+};
+
+struct bad_function_call : std::exception {};
+
 template <typename>
 class function;
 
 template <typename R, typename... Args>
 class function<R(Args...)>
 {
+public: // TO DO: remove
     using invoke_ptr_t = R(*)(void*, Args...);
     using destroy_ptr_t = void(*)(void*);
-    
+    using copy_ptr_t = void*(*)(void*, char*);
+    using move_ptr_t = copy_ptr_t;
+
     char buffer[BUFFER_SIZE]{}; // is alignas required?
-    void* functionObjectPtr;
+    void* functionObjectPtr = nullptr;
     invoke_ptr_t invoke_ptr;
-    destroy_ptr_t destroy_ptr;
+    destroy_ptr_t destroy_ptr; // TO DO: lay pointers in ControlBlock
+    copy_ptr_t copy_ptr;
+    move_ptr_t move_ptr;
 
     template <typename F>
-    static void invoker(F* fPtr, Args... args)
+    static void invokeHelper(F* fPtr, Args... args)
     { // here must be std::invoke
         return (*fPtr)(std::forward<Args>(args)...);
     }
 
     template <typename F>
-    static void destroyer(F* fPtr)
+    static void destroyHelper(F* fPtr)
     {
-        if constexpr (sizeof(F) <= BUFFER_SIZE)
+        if constexpr (sizeof(std::decay_t<F>) <= BUFFER_SIZE) 
         {
             fPtr->~F();
         } else {
             delete fPtr;
         }
     }
-public:
+
     template <typename F>
-    function(F&& f)
-            : functionObjectPtr(nullptr)
-            , invoke_ptr(reinterpret_cast<invoke_ptr_t>(&invoker<F>))
-            , destroy_ptr(reinterpret_cast<destroy_ptr_t>(&destroyer<F>))
+    static void* copyHelper(F* otherFunctionObject, char* bufferPtr)
     {
-        if constexpr (sizeof(f) <= BUFFER_SIZE) // function object is placed in
-                                                // buffer
+        if constexpr (sizeof(F) <= BUFFER_SIZE)
         {
-            new (buffer) F(f);
-            functionObjectPtr = reinterpret_cast<F*>(buffer);
+            new (bufferPtr) F(*otherFunctionObject);
+            return reinterpret_cast<F*>(bufferPtr);
+        }
+        return new F(*otherFunctionObject);
+    }
+
+    template <typename F>
+    static void* moveHelper(F* otherFunctionObject, char* bufferPtr)
+    {
+        if constexpr (sizeof(F) <= BUFFER_SIZE)
+        {
+            new (bufferPtr) F(std::move(*otherFunctionObject));
+            return reinterpret_cast<F*>(bufferPtr);
+        }
+        return new F(std::move(*otherFunctionObject));
+    }
+
+public:
+    using result_type = R;
+    
+    function() noexcept
+            : invoke_ptr(reinterpret_cast<invoke_ptr_t>(nullptr))
+            , destroy_ptr(reinterpret_cast<destroy_ptr_t>(nullptr))
+            , copy_ptr(reinterpret_cast<copy_ptr_t>(nullptr))
+            , move_ptr(reinterpret_cast<move_ptr_t>(nullptr))
+    {}
+
+    function(std::nullptr_t) noexcept : function() {}
+
+    function(const function& other)
+        : invoke_ptr(other.invoke_ptr)
+        , destroy_ptr(other.destroy_ptr)
+        , copy_ptr(other.copy_ptr)
+        , move_ptr(other.move_ptr)
+    {
+        functionObjectPtr = copy_ptr(other.functionObjectPtr, buffer);
+    }
+
+    function(function&& other) noexcept // TO DO: why is it noexcept, if it
+                                        // may call new?
+        : invoke_ptr(other.invoke_ptr)
+        , destroy_ptr(other.destroy_ptr)
+        , copy_ptr(other.copy_ptr)
+        , move_ptr(other.move_ptr)
+    {
+        functionObjectPtr = move_ptr(other.functionObjectPtr, buffer);
+        other.functionObjectPtr = nullptr;
+    }
+
+    template <typename F, std::enable_if_t<!std::is_same_v<
+        F, function<void()>&>, bool> = true>
+    function(F&& f)
+            : invoke_ptr(reinterpret_cast<invoke_ptr_t>(&invokeHelper<
+                std::decay_t<F>>))
+            , destroy_ptr(reinterpret_cast<destroy_ptr_t>(&destroyHelper<
+                std::decay_t<F>>))
+            , copy_ptr(reinterpret_cast<copy_ptr_t>(&copyHelper<
+                std::decay_t<F>>))
+            , move_ptr(reinterpret_cast<move_ptr_t>(&moveHelper<
+                std::decay_t<F>>))
+    {
+        if constexpr (sizeof(std::decay_t<F>) <= BUFFER_SIZE) 
+        { 
+            // function object is placed in buffer
+            new (buffer) std::decay_t<F>(f);
+            functionObjectPtr = reinterpret_cast<void*>(buffer);
         } else { // function object is placed in dynamic memory
-            functionObjectPtr = new F(f);
+            functionObjectPtr = new std::decay_t<F>(f);
         }
     }
 
     R operator()(Args... args) const
     {
+        if (!functionObjectPtr) [[unlikely]] 
+        {
+            throw bad_function_call("The function is empty\n");
+        }
         return invoke_ptr(functionObjectPtr, std::forward<Args>(args)...);
     }
 
     ~function()
     {
         destroy_ptr(functionObjectPtr);
+    }
+
+    void print() const noexcept // TO DO: remove
+    {
+        std::cout << "buffer: ptr: " << reinterpret_cast<const void*>(buffer) 
+            << ' ' << " content: ";
+        for (size_t i = 0; i < BUFFER_SIZE; ++i)
+        {
+            std::cout << std::hex << (static_cast<uint16_t>(buffer[i]) & 0xff)
+                << ' ';
+        }
+        std::cout << std::dec << "\nfunctionObjectPtr: " << functionObjectPtr 
+            << '\n';
+        std::cout << "invoke_ptr: " << reinterpret_cast<void*>(invoke_ptr) << 
+            std::endl;
+        std::cout << "destroy_ptr: " << reinterpret_cast<void*>(destroy_ptr) <<
+            std::endl;
+        std::cout << "copy_ptr: " << reinterpret_cast<void*>(copy_ptr) <<
+            std::endl;
+        std::cout << "move_ptr: " << reinterpret_cast<void*>(move_ptr) <<
+            std::endl;
     }
 };
 
--- C++/function/main.cpp
+++ C++/function/main.cpp
@@ -1,4 +1,5 @@
 #include "function.hpp"
+#include <functional>
 
 struct BigObject
 {
@@ -8,34 +9,104 @@ struct BigObject
     void operator()() const { std::cout << x1 << x2 << x3 << '\n'; }
 };
 
-void* operator new(size_t n)
-{ 
-    void* ret = malloc(n);
-    std::cout << n << " bytes allocated at " << ret << std::endl;
-    return ret;
-}
-
-void operator delete(void* pObject)
+struct SmallObject
 {
-    std::cout << "delete at " << pObject << std::endl;
-    free(pObject);
-}
+    void operator()() const { std::cout << 123 << '\n'; }
+};
 
-void operator delete(void* pObject, size_t n) 
-{ 
-    std::cout << "delete " << n << " bytes at " << pObject << std::endl;
-    free(pObject); 
-}
+//void* operator new(size_t n)
+//{ 
+//    void* ret = malloc(n);
+//    std::cout << n << " bytes allocated at " << ret << std::endl;
+//    return ret;
+//}
+//
+//void operator delete(void* pObject)
+//{
+//    std::cout << "delete at " << pObject << std::endl;
+//    free(pObject);
+//}
+//
+//void operator delete(void* pObject, size_t n) 
+//{ 
+//    std::cout << "delete " << n << " bytes at " << pObject << std::endl;
+//    free(pObject); 
+//}
 
 void cStyleFunction() { std::cout << 123 << '\n'; }
 
 int main()
 {
-    function<void()> func1 = BigObject();
-    func1();
-    function<void()> func2 = []{ std::cout << 123; };
-    func2();
-    function<void()> func3 = cStyleFunction();
-    func3();
+    std::function<void()> f1 = [] { std::cout << 123 << '\n'; };
+    std::function<void()> f2 = BigObject();
+    std::function<void()> f3 = SmallObject();
+    std::function<void()> f4 = &cStyleFunction;
+    std::function<void()> f5 = cStyleFunction;
+    std::function<void()> fTest1 = f1;
+    fTest1();
+    std::function<void()> fTest2 = f2;
+    fTest2();
+    std::function<void()> fTest3 = f3;
+    fTest3();
+    std::function<void()> fTest4 = f4;
+    fTest4();
+    std::function<void()> fTest5 = f5;
+    fTest5();
+    std::cout << std::endl;
+    std::function<void()> fTest6 = std::move(f1);
+    fTest6();
+    std::function<void()> fTest7 = std::move(f2);
+    fTest7();
+    std::function<void()> fTest8 = std::move(f3);
+    fTest8();
+    std::function<void()> fTest9 = std::move(f4);
+    fTest9();
+    std::function<void()> fTest10 = std::move(f5);
+    fTest10();
+    std::cout << std::endl;
+    try
+    {
+        f1();
+    } catch (...) {
+        std::cout << "Exception caught\n";
+    }
+    try
+    {
+        f2();
+    } catch (...) {
+        std::cout << "Exception caught\n";
+    }
+    try
+    {
+        f3();
+    } catch (...) {
+        std::cout << "Exception caught\n";
+    }
+    try
+    {
+        f4();
+    } catch (...) {
+        std::cout << "Exception caught\n";
+    }
+    try
+    {
+        f5();
+    } catch (...) {
+        std::cout << "Exception caught\n";
+    }
 }
+
+//int main()
+//{
+//    function<void()> f1 = []() { std::cout << 123 << '\n'; };
+//    f1();
+//    function<void()> f2 = BigObject{};
+//    f2();
+//    function<void()> f3 = SmallObject{};
+//    f3();
+//    function<void()> f4 = &cStyleFunction;
+//    f4();
+//    function<void()> f5 = cStyleFunction;
+//    f5();
+//}
 
